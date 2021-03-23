f518ac20c01a8a80cfcb551c20e663a2390a737f
Bryce Kalmbach <jbkalmbach@gmail.com>
2021-03-23 12:06:57 -0700

    Updated ZernikeEstimateTask to use donutStamps.

--- python/lsst/ts/wep/ZernikeEstimateTask.py
+++ python/lsst/ts/wep/ZernikeEstimateTask.py
@@ -4,6 +4,7 @@ import lsst.pipe.base as pipeBase
 import lsst.pex.config as pexConfig
 import lsst.afw.table as afwTable
 import lsst.afw.image as afwImage
+from lsst.meas.algorithms import Stamps
 
 import os
 import typing
@@ -14,28 +15,42 @@ from lsst.ts.wep.Utility import getConfigDir, DefocalType
 from lsst.pipe.base import connectionTypes
 
 
-class ZernikeEstimateTaskConnections(pipeBase.PipelineTaskConnections,
-                                     dimensions=("detector", "instrument")):
-    exposures = connectionTypes.Input(doc="Input exposure to make measurements "
-                                          "on",
-                                      dimensions=("exposure", "detector", "instrument"),
-                                      storageClass="Exposure",
-                                      name="postISRCCD",
-                                      multiple=True)
-    inputCatalogs = connectionTypes.Input(doc="Input donut location catalog",
-                                          dimensions=("exposure", "detector", "instrument"),
-                                          storageClass="SourceCatalog",
-                                          name="donutCatalog",
-                                          multiple=True)
-    outputZernikes = connectionTypes.Output(doc="Zernike Coefficients",
-                                            dimensions=("exposure", "detector", "instrument"),
-                                            storageClass="NumpyArray",
-                                            name="zernikeEstimate")
+class ZernikeEstimateTaskConnections(
+    pipeBase.PipelineTaskConnections, dimensions=("detector", "instrument")
+):
+    exposures = connectionTypes.Input(
+        doc="Input exposure to make measurements on",
+        dimensions=("exposure", "detector", "instrument"),
+        storageClass="Exposure",
+        name="postISRCCD",
+        multiple=True,
+    )
+    donutStamps = connectionTypes.Input(
+        doc="Input exposure to make measurements on",
+        dimensions=("exposure", "detector", "instrument"),
+        storageClass="Stamps",
+        name="donutStamps",
+        multiple=True,
+    )
+    donutCatalogs = connectionTypes.Input(
+        doc="Input donut location catalog",
+        dimensions=("exposure", "detector", "instrument"),
+        storageClass="SimpleCatalog",
+        name="donutCatalog",
+        multiple=True,
+    )
+    outputZernikes = connectionTypes.Output(
+        doc="Zernike Coefficients",
+        dimensions=("exposure", "detector", "instrument"),
+        storageClass="NumpyArray",
+        name="zernikeEstimate",
+    )
 
 
-class ZernikeEstimateTaskConfig(pipeBase.PipelineTaskConfig,
-                                pipelineConnections=ZernikeEstimateTaskConnections):
-    donutImageSize = pexConfig.Field(doc="Size of Template", dtype=int, default=160)
+class ZernikeEstimateTaskConfig(
+    pipeBase.PipelineTaskConfig, pipelineConnections=ZernikeEstimateTaskConnections
+):
+    donutImageSize = pexConfig.Field(doc="Size of Stamp", dtype=int, default=160)
 
 
 class ZernikeEstimateTask(pipeBase.PipelineTask):
@@ -47,32 +62,39 @@ class ZernikeEstimateTask(pipeBase.PipelineTask):
         super().__init__(*args, **kwargs)
         self.donutImageSize = self.config.donutImageSize
 
-    def run(self, exposures: typing.List[afwImage.Exposure],
-            inputCatalogs: typing.List[afwTable.SourceCatalog]) -> pipeBase.Struct:
+    def run(
+        self,
+        exposures: typing.List[afwImage.Exposure],
+        donutStamps: typing.List[Stamps],
+        donutCatalogs: typing.List[afwTable.SourceCatalog],
+    ) -> pipeBase.Struct:
+
+        zerArray = []
 
         configDir = getConfigDir()
-        instDir = os.path.join(configDir, 'cwfs', 'instData')
-        algoDir = os.path.join(configDir, 'cwfs', 'algo')
+        instDir = os.path.join(configDir, "cwfs", "instData")
+        algoDir = os.path.join(configDir, "cwfs", "algo")
         wfEsti = WfEstimator(instDir, algoDir)
         wfEsti.config(sizeInPix=self.donutImageSize)
         sourProc = SourceProcessor()
         sourProc.config(sensorName=exposures[0].getDetector().getName())
-        fieldXY = sourProc.camXYtoFieldXY(inputCatalogs[0][0]['xPixel'],
-                                          inputCatalogs[0][0]['yPixel'])
 
-        stampWidth = int(self.donutImageSize / 2)
-        donutStamps = []
-        for exposure, inputCatalog in zip(exposures, inputCatalogs):
-            xLoc = int(inputCatalog[0]['xPixel'])
-            yLoc = int(inputCatalog[0]['yPixel'])
-            donutCutout = exposure.image.array.T[xLoc-stampWidth:xLoc+stampWidth,
-                                                 yLoc-stampWidth:yLoc+stampWidth]
-            donutStamps.append(donutCutout)
+        extraImages = donutStamps[0].getMaskedImages()
+        intraImages = donutStamps[1].getMaskedImages()
 
-        wfEsti.setImg(fieldXY, DefocalType.Extra, image=donutStamps[0])
-        wfEsti.setImg(fieldXY, DefocalType.Intra, image=donutStamps[1])
-        wfEsti.reset()
-        zer4UpNm = wfEsti.calWfsErr()
-        zer4UpMicrons = zer4UpNm * 1e-3
+        for idx in range(len(donutCatalogs[0])):
+            fieldXY = sourProc.camXYtoFieldXY(
+                donutCatalogs[0][idx]["centroid_x"], donutCatalogs[0][idx]["centroid_y"]
+            )
 
-        return pipeBase.Struct(outputZernikes=np.array(zer4UpMicrons))
+            wfEsti.setImg(fieldXY, DefocalType.Extra,
+                          image=extraImages[idx].image.array)
+            wfEsti.setImg(fieldXY, DefocalType.Intra,
+                          image=intraImages[idx].image.array)
+            wfEsti.reset()
+            zer4UpNm = wfEsti.calWfsErr()
+            zer4UpMicrons = zer4UpNm * 1e-3
+
+            zerArray.append(zer4UpMicrons)
+
+        return pipeBase.Struct(outputZernikes=np.array(zerArray))
