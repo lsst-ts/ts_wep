Fixing units in PairTask

python/lsst/ts/wep/task/pairTask.py
@@ -133,7 +133,7 @@ class ExposurePairer(pipeBase.Task):
                 visitInfo.boresightParAngle
                 - visitInfo.boresightRotAngle
                 - (np.pi / 2 * radians)
-            ).asRadians()
+            ).asDegrees()
             focusZ = visitInfo.focusZ
             table.add_row([exposure, ra, dec, mjd, focusZ, rtp])
         table["radec"] = SkyCoord(table["ra"], table["dec"], unit="radian")
@@ -177,11 +177,11 @@ class ExposurePairer(pipeBase.Task):
                 < self.config.timeThreshold
             )
             nearby &= (
-                np.abs(intraTable["rtp"] - row["rtp"]) * 3600
+                np.abs(intraTable["rtp"] - row["rtp"])
                 < self.config.rotationThreshold
             )
             nearby &= (
-                row["radec"].separation(intraTable["radec"]).deg
+                row["radec"].separation(intraTable["radec"]).deg * 3600
                 < self.config.pointingThreshold
             )
             if np.any(nearby):
@@ -222,6 +222,9 @@ class ExposurePairer(pipeBase.Task):
         tables = self.makeTables(visitInfos)
         pairTable = tables["pairTable"]
         out = []
+
+        if len(pairTable) == 0:
+            self.log.warning("Error Running PairTask: No pairs found.")
         for row in pairTable:
             out.append(IntraExtraIdxPair(row["intra"], row["extra"]))
         return out