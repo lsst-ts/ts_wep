96f46951de2c57961f39402392057e7687cf7933
J. Bryce Kalmbach <jbkalmbach@gmail.com>
2026-09-02 12:25:30 -0700

    Bring license up to compatibility with v0.10.2 of ts_pre_commit_conf.

--- bin.src/generatePairTable.py
+++ bin.src/generatePairTable.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-# This file is part of ts-wep.
+# This file is part of ts_wep.
 #
 # Developed for the Vera C. Rubin Observatory Telescope and Site Systems.
 # This product includes software developed by the LSST Project
--- python/lsst/ts/wep/bin/ingestPairTable.py
+++ python/lsst/ts/wep/bin/ingestPairTable.py
@@ -1,4 +1,4 @@
-# This file is part of ts-wep.
+# This file is part of ts_wep.
 #
 # Developed for the Vera C. Rubin Observatory Telescope and Site Systems.
 # This product includes software developed by the LSST Project
