5177d1a337703aa5da7a873bcd32d1674b9fdfa8
Josh Meyers <jmeyers3@slac.stanford.edu>
2026-09-10 08:32:09 -0700

    --no-edit

--- python/lsst/ts/wep/task/__init__.py
+++ python/lsst/ts/wep/task/__init__.py
@@ -40,7 +40,6 @@ from .estimateZernikesTieTask import *
 from .generateDonutCatalogOnlineTask import *
 from .generateDonutCatalogUtils import *
 from .generateDonutCatalogWcsTask import *
-from .generateDonutBlitzDetectTask import *
 from .generateDonutDirectDetectTask import *
 from .generateDonutFromRefitWcsTask import *
 from .latissMonolithTask import *
